dbb0a8069c33f4d5c566ae046ea7fc9be118312d
David Edson Do Nascimento <david.contato.tec@gmail.com>
2018-12-02 17:08:40 -0200

    segundo commit

---
+++ ReggaetonDale/Controllers/CadastrosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReggaetonDale.Controllers
+{
+    public class CadastrosController : Controller
+    {
+        // GET: Cadastros
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Carreira()
+        {
+            return View("Carreira");
+        }
+
+        public ActionResult UsuarioCarreira()
+        {
+            return View("UsuarioCarreira");
+        }
+
+    }
+}
---
+++ ReggaetonDale/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReggaetonDale.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Carreiras()
+        {
+            return View();
+        }
+        public ActionResult Conteudo()
+        {
+            return View();
+        }
+        public ActionResult Relatorio()
+        {
+            return View();
+        }
+    }
+}
